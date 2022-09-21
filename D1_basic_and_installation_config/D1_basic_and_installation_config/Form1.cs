d4d702aa5c48d232fe6926979c8a45c06d56f270
郵差 <twHenryYang@gmail.com>
2022-09-21 11:32:50 +0800

    update: generate Random number for practice1

--- D1_basic_and_installation_config/D1_basic_and_installation_config/Form1.cs
+++ D1_basic_and_installation_config/D1_basic_and_installation_config/Form1.cs
@@ -24,10 +24,13 @@ namespace D1_basic_and_installation_config
 
         private void practice1Btn_Click(object sender, EventArgs e)
         {
+            Random rnd = new Random();
             int pen = 23;
             double watch = 200.2;
+            double landPrice = rnd.NextDouble() * 100;
             string log = String.Format(
-             "一枝筆賣{0}元，一支錶要價{1}元", pen, watch);
+             "一枝筆賣{0}元，一支錶要價{1}元，隨機估計大安區每坪地價為{2}萬元",
+             pen, watch, landPrice);
             MessageBox.Show(log);
         }
     }
