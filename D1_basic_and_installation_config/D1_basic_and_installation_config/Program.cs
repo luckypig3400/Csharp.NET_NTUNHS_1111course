bd1dc1cf2c3e6740f4aecf59d26bfa60a54bbe0e
郵差 <twHenryYang@gmail.com>
2022-09-21 11:15:36 +0800

    fix: High DPI Screen GUI fuzzy issue
    https://docs.google.com/presentation/d/1rmGgxOtJeg2HnomUvsrXdHhg0OTF8h4N/edit#slide=id.g15ac3dcb161_0_0

--- D1_basic_and_installation_config/D1_basic_and_installation_config/Program.cs
+++ D1_basic_and_installation_config/D1_basic_and_installation_config/Program.cs
@@ -14,9 +14,18 @@ namespace D1_basic_and_installation_config
         [STAThread]
         static void Main()
         {
+            // high dpi start
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+            // high dpi end
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        // high dpi start
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+        // high dpi end
     }
 }
