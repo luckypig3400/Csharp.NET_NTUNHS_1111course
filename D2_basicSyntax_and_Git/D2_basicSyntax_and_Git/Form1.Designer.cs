6243d192570345a943139e1b5cf7f3f3d9d9ce51
郵差 <twHenryYang@gmail.com>
2022-10-05 10:07:02 +0800

    feat: finish practice1 Show My BMI

--- D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.Designer.cs
+++ D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.p1BtnBMI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // p1BtnBMI
+            // 
+            this.p1BtnBMI.Font = new System.Drawing.Font("新細明體", 12F);
+            this.p1BtnBMI.Location = new System.Drawing.Point(59, 34);
+            this.p1BtnBMI.Name = "p1BtnBMI";
+            this.p1BtnBMI.Size = new System.Drawing.Size(106, 61);
+            this.p1BtnBMI.TabIndex = 0;
+            this.p1BtnBMI.Text = "練習一: 我的BMI";
+            this.p1BtnBMI.UseVisualStyleBackColor = true;
+            this.p1BtnBMI.Click += new System.EventHandler(this.p1BtnBMI_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.p1BtnBMI);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button p1BtnBMI;
     }
 }
 
--- D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.cs
+++ D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.cs
@@ -16,5 +16,18 @@ namespace D2_basicSyntax_and_Git
         {
             InitializeComponent();
         }
+
+        private void p1BtnBMI_Click(object sender, EventArgs e)
+        {
+            double weight = 76.9;
+            double height = 1.69;
+            double bmi = weight / (height * height);
+            DateTime today = DateTime.Now;
+
+            string log = String.Format(
+                "今天是{0}, 我的BMI為{1}",
+                today.ToString("yyyy-MM-dd"), bmi);
+            MessageBox.Show(log, "練習1:我的BMI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
