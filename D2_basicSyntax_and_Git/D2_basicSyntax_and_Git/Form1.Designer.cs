775f3316e977cbc70d6493a9938be8e0fd0625b9
郵差 <twHenryYang@gmail.com>
2022-10-05 10:32:34 +0800

    feat: Add WebBrowser Component

--- D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.Designer.cs
+++ D2_basicSyntax_and_Git/D2_basicSyntax_and_Git/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.p1BtnBMI = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // p1BtnBMI
             // 
-            this.p1BtnBMI.Font = new System.Drawing.Font("新細明體", 12F);
+            this.p1BtnBMI.Font = new System.Drawing.Font("PMingLiU", 12F);
             this.p1BtnBMI.Location = new System.Drawing.Point(59, 34);
             this.p1BtnBMI.Name = "p1BtnBMI";
             this.p1BtnBMI.Size = new System.Drawing.Size(106, 61);
@@ -42,11 +43,21 @@
             this.p1BtnBMI.UseVisualStyleBackColor = true;
             this.p1BtnBMI.Click += new System.EventHandler(this.p1BtnBMI_Click);
             // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 111);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 327);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://github.com/", System.UriKind.Absolute);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.p1BtnBMI);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button p1BtnBMI;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
