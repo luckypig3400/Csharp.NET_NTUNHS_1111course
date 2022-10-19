0e163ebf5badcc5a32e066256aef1b98b1ca0540
郵差 <twHenryYang@gmail.com>
2022-10-19 10:10:11 +0800

    feat: Add 5 Labels, 2 TextBoxes & 1 RichTextBox

--- D04_Basic_GUI_Design/D04_Basic_GUI_Design/Form1.Designer.cs
+++ D04_Basic_GUI_Design/D04_Basic_GUI_Design/Form1.Designer.cs
@@ -30,12 +30,20 @@
         {
             this.inputDataBtn = new System.Windows.Forms.Button();
             this.outputDataBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // inputDataBtn
             // 
             this.inputDataBtn.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.inputDataBtn.Location = new System.Drawing.Point(25, 588);
+            this.inputDataBtn.Location = new System.Drawing.Point(33, 461);
             this.inputDataBtn.Name = "inputDataBtn";
             this.inputDataBtn.Size = new System.Drawing.Size(147, 54);
             this.inputDataBtn.TabIndex = 0;
@@ -45,23 +53,106 @@
             // outputDataBtn
             // 
             this.outputDataBtn.Font = new System.Drawing.Font("PMingLiU", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.outputDataBtn.Location = new System.Drawing.Point(195, 588);
+            this.outputDataBtn.Location = new System.Drawing.Point(186, 461);
             this.outputDataBtn.Name = "outputDataBtn";
             this.outputDataBtn.Size = new System.Drawing.Size(147, 54);
             this.outputDataBtn.TabIndex = 1;
             this.outputDataBtn.Text = "輸出資料";
             this.outputDataBtn.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(58, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "編號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(58, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "貨品";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(58, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "單價";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(58, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "數量";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("PMingLiU", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.idLabel.Location = new System.Drawing.Point(181, 41);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(32, 28);
+            this.idLabel.TabIndex = 6;
+            this.idLabel.Text = "--";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("PMingLiU", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(153, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("PMingLiU", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(153, 206);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 35);
+            this.textBox2.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 267);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(277, 171);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.outputDataBtn);
             this.Controls.Add(this.inputDataBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +160,14 @@
 
         private System.Windows.Forms.Button inputDataBtn;
         private System.Windows.Forms.Button outputDataBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
