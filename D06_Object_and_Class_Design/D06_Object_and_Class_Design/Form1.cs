31ce4c6d92dde313a30f015dca453a5663c275a4
郵差 <twHenryYang@gmail.com>
2022-11-02 10:25:55 +0800

    update: Without Class & Object write style

--- D06_Object_and_Class_Design/D06_Object_and_Class_Design/Form1.cs
+++ D06_Object_and_Class_Design/D06_Object_and_Class_Design/Form1.cs
@@ -16,5 +16,22 @@ namespace D06_Object_and_Class_Design
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int employee_1_id = 1;
+            String employee_1_name = "Tom";
+            int employee_2_id = 2;
+            String employee_2_name = "John";
+            int employee_3_id = 3;
+            String employee_3_name = "Joe";
+
+            MessageBox.Show(String.Format("id: {0}, name: {1}"
+                                , employee_1_id, employee_1_name));
+            MessageBox.Show(String.Format("id: {0}, name: {1}"
+                                , employee_2_id, employee_2_name));
+            MessageBox.Show(String.Format("id: {0}, name: {1}"
+                                , employee_3_id, employee_3_name));
+        }
     }
 }
