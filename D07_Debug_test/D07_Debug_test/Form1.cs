8e201a6a5a18e11f7829e03aabe292575e69a5d3
郵差 <twHenryYang@gmail.com>
2022-11-16 10:46:32 +0800

    feat: add Assert code

--- D07_Debug_test/D07_Debug_test/Form1.cs
+++ D07_Debug_test/D07_Debug_test/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace D07_Debug_test
 {
@@ -19,6 +20,19 @@ namespace D07_Debug_test
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int i = 4;
+
+            // Debug.Assert 是用在 Debug mode，在release mode中，不會執行Assert
+            // i應該要等於4，否則跳出警訊
+            Debug.Assert(i == 4);
+            // 檢查i是等於4：會輸出 True Debug: i is not equal to 4
+            Debug.WriteLine(i == 4, "Debug: i is not equal to 4");
+
+            // Trace.Assert 是用在 Debug mode和release mode
+            // i應該要等於4，否則跳出警訊
+            Trace.Assert(i == 4);
+            // 檢查i是等於4：會輸出 True Debug: i is not equal to 4
+            Trace.WriteLine(i == 4, "Trace: i is equal to 4");
 
         }
     }
