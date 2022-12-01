35fa3cd2c137811287fee571107e1e912e6d5832
郵差 <twHenryYang@gmail.com>
2022-12-01 10:09:34 +0800

    feat: D07 create Unit Test

---
+++ D07_Debug_test/D07_Debug_testTests/Form1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using D07_Debug_test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D07_Debug_test.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
