033798d394e23361b73048033f094010a7c972bc
郵差 <twHenryYang@gmail.com>
2022-12-01 10:14:12 +0800

    update: D07 unit test code

--- D07_Debug_test/D07_Debug_testTests/Form1Tests.cs
+++ D07_Debug_test/D07_Debug_testTests/Form1Tests.cs
@@ -12,9 +12,36 @@ namespace D07_Debug_test.Tests
     public class Form1Tests
     {
         [TestMethod()]
+        public void Form1Test()
+        {
+            Assert.AreEqual(1, 1);
+            Assert.AreEqual(true, true);
+            Assert.AreEqual("string", "string");
+            Assert.AreEqual(1.0, 1.0);
+            Assert.AreEqual(1.4, // expected value
+                            1.62, // actual value
+                            2.3, // delta 忍受誤差範圍
+                            "error (delta=2.3) " // error message，錯誤時會跳出這個訊息
+                            );
+            /*
+            Assert.AreEqual(1.4, // expected value
+                            1.62, // actual value
+                            0.1, // delta 忍受誤差範圍
+                            "error (delta=0.1) " // error message，錯誤時會跳出這個訊息
+                            );
+            */
+        }
+
+        [TestMethod()]
         public void AddTest()
         {
-            Assert.Fail();
+            double expected = 6.0; // TODO: 初始化為適當值
+            double actual;
+
+            Form1 test = new Form1();
+            actual = test.Add(2.0, 4.0);
+            Assert.AreEqual(expected, actual);
         }
+
     }
 }
