b6b501b667174cb4ee5aeac060629bbb4da81d9c
郵差 <twHenryYang@gmail.com>
2022-12-07 11:39:35 +0800

    feat: load *.dll file function
    https://docs.google.com/document/d/1EQJJYe9WS29ShZBpWnAUtVL3tc7_fD5e/edit#heading=h.gp0ck6h3ywgc

--- D08_Dynamic_Binding_Multi-projects/mproject_dynamic_binding/mproject_dynamic_binding/Form1.cs
+++ D08_Dynamic_Binding_Multi-projects/mproject_dynamic_binding/mproject_dynamic_binding/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//dynamic binding
+using System.Reflection;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace mproject_dynamic_binding
 {
     public partial class Form1 : Form
@@ -19,9 +23,11 @@ namespace mproject_dynamic_binding
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // 功能1
+            // 1: 初始化邊長
             double edge = 2.3;
 
+            // 2: 計算
+            /*
             // 功能2
             //double rectangular = edge * edge;
             iMeasurement cal_rect = new rect();
@@ -31,10 +37,47 @@ namespace mproject_dynamic_binding
             //double cube = edge * edge * edge;
             iMeasurement cal_cube = new cube();
             double cube_result = cal_cube.cal(edge);
+            */
 
-            // 功能4
-            MessageBox.Show(String.Format("正方型面積 {0}, 正立方體面積 {1}",
-                                                                       rect_result, cube_result));
+            Assembly assembly;
+            Type type;
+
+            // 2.1. 找出要動態綁定(dynamic binding)的函式庫(dll)檔案位置
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select dll file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "dll files (*.*)|*.dll";
+            string dll_path = "";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                dll_path = dialog.FileName;
+            }
+
+            // 2.2. 載入函式庫(dll)檔案
+            assembly = Assembly.LoadFrom(dll_path);
+
+            // 2.3. 找出要載入的class名稱(格式: namesapce_name.class_name)
+            // type = assembly.GetType("Team2.rect");
+            //type = assembly.GetType(textBox1.Text);
+            type = assembly.GetType(dll_path);
+
+            // 2.4. 初始化物件
+            var dynamic_object = Activator.CreateInstance(type);
+
+            // 2.5. 初始化方法
+            var dynamic_method = type.GetMethod("cal", new Type[] { typeof(double) });
+
+            // 2.6. 呼叫函示
+            var dynamic_result = dynamic_method.Invoke(dynamic_object, // 第一個是物件
+                                 new object[] { edge } // 第二個是要輸入函式的參數
+                                 );
+
+            // 3: 跳出正方形面積/體積的資料
+            //MessageBox.Show(String.Format("正方型面積 {0}, 正立方體面積 {1}",
+            //                                 rect_result, cube_result));
+
+            MessageBox.Show(dynamic_result.ToString());
+
         }
     }
 
