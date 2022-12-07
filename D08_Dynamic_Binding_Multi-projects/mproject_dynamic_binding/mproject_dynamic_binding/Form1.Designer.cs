5871ec1dce62aa7e9997399e8287c3dc9fb7fa57
郵差 <twHenryYang@gmail.com>
2022-12-07 13:37:48 +0800

    chore: GUI for D08-6. 補充教材2: 動態抽換還沒編譯好的程式碼

--- D08_Dynamic_Binding_Multi-projects/mproject_dynamic_binding/mproject_dynamic_binding/Form1.Designer.cs
+++ D08_Dynamic_Binding_Multi-projects/mproject_dynamic_binding/mproject_dynamic_binding/Form1.Designer.cs
@@ -29,24 +29,46 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(125, 127);
+            this.button1.Location = new System.Drawing.Point(12, 330);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(565, 161);
+            this.button1.Size = new System.Drawing.Size(313, 108);
             this.button1.TabIndex = 0;
-            this.button1.Text = "真正的Dynamic Binding抽換計算正方形的程式碼";
+            this.button1.Text = "5. 補充教材1: Dynamic Binding-在執行程式時，動態抽換已編譯好的程式碼(DLL檔)";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 312);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(385, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(403, 108);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "點我計算\r\n(6. 補充教材2: Dynamic Binding-在執行程式時，動態抽換還沒編譯好的程式碼)";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
