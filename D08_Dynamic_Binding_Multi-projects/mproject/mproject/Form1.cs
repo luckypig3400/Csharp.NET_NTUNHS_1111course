673e899b17f457dc600c19bdd728609a56ff8afd
郵差 <twHenryYang@gmail.com>
2022-12-07 10:23:00 +0800

    feat: D08-1. 用物件導向的方式，將計算工作拆分給新團隊

--- D08_Dynamic_Binding_Multi-projects/mproject/mproject/Form1.cs
+++ D08_Dynamic_Binding_Multi-projects/mproject/mproject/Form1.cs
@@ -23,14 +23,56 @@ namespace mproject
             double edge = 2.3;
 
             // 功能2: 計算正方形面積
-            double rect_result = edge * edge;
+            //double rect_result = edge * edge;
+            iMeasurement cal_rect = new rect();
+            double rect_result = cal_rect.cal(edge);
 
             // 功能3: 計算正方體體積
-            double cube_result = edge * edge * edge;
+            //double cube_result = edge * edge * edge;
+            iMeasurement cal_cube = new cube();
+            double cube_result = cal_cube.cal(edge);
 
             // 功能4: 跳出正方形面積與體積的資料
             MessageBox.Show(String.Format("正方型面積 {0}, 正立方體面積 {1}",
                                              rect_result, cube_result));
         }
     }
+
+    // for 團隊一
+    // for contract (契約)
+    public interface iMeasurement
+    {
+        double cal(double edge);
+    }
+
+    // for 團隊二
+    public class rect : iMeasurement
+    {
+        public double measurement;
+        public rect()
+        {
+            measurement = 0;
+        }
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge;
+            return measurement;
+        }
+    }
+
+    // for 團隊三
+    public class cube : iMeasurement
+    {
+        public double measurement;
+        public cube()
+        {
+            measurement = 0;
+        }
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge * i_edge;
+            return measurement;
+        }
+    }
+
 }
