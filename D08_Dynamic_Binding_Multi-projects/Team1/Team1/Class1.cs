e97c8f336e1bb7358b08161e91aacc1342b2a6e5
郵差 <twHenryYang@gmail.com>
2022-12-07 11:02:21 +0800

    feat: Separate Team1 code to Team2

--- D08_Dynamic_Binding_Multi-projects/Team1/Team1/Class1.cs
+++ D08_Dynamic_Binding_Multi-projects/Team1/Team1/Class1.cs
@@ -13,37 +13,4 @@ namespace Team1
         double cal(double edge);
     }
 
-    // for 團隊二
-    public class rect : iMeasurement
-    {
-        public double measurement;
-
-        public rect()
-        {
-            measurement = 0;
-        }
-
-        public double cal(double i_edge)
-        {
-            measurement = i_edge * i_edge;
-            return measurement;
-        }
-    }
-
-    // for 團隊三
-    public class cube : iMeasurement
-    {
-        public double measurement;
-
-        public cube()
-        {
-            measurement = 0;
-        }
-        public double cal(double i_edge)
-        {
-            measurement = i_edge * i_edge * i_edge;
-            return measurement;
-        }
-    }
-
 }
---
+++ D08_Dynamic_Binding_Multi-projects/Team2/Team2/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team1;
+
+namespace Team2
+{
+    // for 團隊二
+    public class rect : iMeasurement
+    {
+        public double measurement;
+
+        public rect()
+        {
+            measurement = 0;
+        }
+
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge;
+            return measurement;
+        }
+    }
+
+    // for 團隊三
+    public class cube : iMeasurement
+    {
+        public double measurement;
+
+        public cube()
+        {
+            measurement = 0;
+        }
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge * i_edge;
+            return measurement;
+        }
+    }
+}
