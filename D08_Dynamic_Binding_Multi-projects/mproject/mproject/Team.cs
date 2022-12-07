2a9c501303f0e126c8512a929afb981ee1d3f3d8
郵差 <twHenryYang@gmail.com>
2022-12-07 10:26:16 +0800

    feat: D08-2. 把分工出去的程式碼，獨立成一個新檔案
    https://docs.google.com/document/d/1EQJJYe9WS29ShZBpWnAUtVL3tc7_fD5e/edit#heading=h.y598wnl5nhsa

---
+++ D08_Dynamic_Binding_Multi-projects/mproject/mproject/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace mproject
+{
+    // for 團隊一
+    // for contract (契約)
+    public interface iMeasurement
+    {
+        double cal(double edge);
+    }
+
+    // for 團隊二
+    public class rect : iMeasurement
+    {
+        public double measurement;
+        public rect()
+        {
+            measurement = 0;
+        }
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge;
+            return measurement;
+        }
+    }
+
+    // for 團隊三
+    public class cube : iMeasurement
+    {
+        public double measurement;
+        public cube()
+        {
+            measurement = 0;
+        }
+        public double cal(double i_edge)
+        {
+            measurement = i_edge * i_edge * i_edge;
+            return measurement;
+        }
+    }
+
+}
