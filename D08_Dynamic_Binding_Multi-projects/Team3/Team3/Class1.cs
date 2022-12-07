44d66e2d9ae6f5272e412b25306a730784973696
郵差 <twHenryYang@gmail.com>
2022-12-07 11:11:50 +0800

    feat: D08 Separate Team2 code to Team3 project

--- D08_Dynamic_Binding_Multi-projects/Team3/Team3/Class1.cs
+++ D08_Dynamic_Binding_Multi-projects/Team3/Team3/Class1.cs
@@ -5,23 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Team1;
 
-namespace Team2
+namespace Team3
 {
-    // for 團隊二
-    public class rect : iMeasurement
+    // for 團隊三
+    public class cube : iMeasurement
     {
         public double measurement;
 
-        public rect()
+        public cube()
         {
             measurement = 0;
         }
-
         public double cal(double i_edge)
         {
-            measurement = i_edge * i_edge;
+            measurement = i_edge * i_edge * i_edge;
             return measurement;
         }
     }
-
 }
