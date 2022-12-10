7366c5b2c4e8122bbb825563aaf693c9fb3bead3
郵差 <twHenryYang@gmail.com>
2022-12-10 15:29:32 +0800

    Create D10-1 Read/Write Excel, CSV and JSON

---
+++ D10_Export_excel_csv_and_chart/D10-1_Read_Write_Excel_CSV_JSON/D10-1_Read_Write_Excel_CSV_JSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D10_1_Read_Write_Excel_CSV_JSON
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
