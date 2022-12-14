122bbefa0617deb44b4317df8ff73c809d2d0824
郵差 <twHenryYang@gmail.com>
2022-12-14 18:29:07 +0800

    style: Add more charts in D10-2 GUI

--- D10_Export_excel_csv_and_chart/D10-2_Statistic_Chart/D10-2_Statistic_Chart/Form1.Designer.cs
+++ D10_Export_excel_csv_and_chart/D10-2_Statistic_Chart/D10-2_Statistic_Chart/Form1.Designer.cs
@@ -31,17 +31,27 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.drawChartBTN = new System.Windows.Forms.Button();
             this.exportImageBTN = new System.Windows.Forms.Button();
             this.exportExcelBTN = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // drawChartBTN
             // 
             this.drawChartBTN.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.drawChartBTN.Location = new System.Drawing.Point(12, 472);
+            this.drawChartBTN.Location = new System.Drawing.Point(821, 500);
             this.drawChartBTN.Name = "drawChartBTN";
             this.drawChartBTN.Size = new System.Drawing.Size(240, 69);
             this.drawChartBTN.TabIndex = 0;
@@ -52,7 +62,7 @@
             // exportImageBTN
             // 
             this.exportImageBTN.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.exportImageBTN.Location = new System.Drawing.Point(270, 472);
+            this.exportImageBTN.Location = new System.Drawing.Point(821, 575);
             this.exportImageBTN.Name = "exportImageBTN";
             this.exportImageBTN.Size = new System.Drawing.Size(240, 69);
             this.exportImageBTN.TabIndex = 1;
@@ -63,7 +73,7 @@
             // exportExcelBTN
             // 
             this.exportExcelBTN.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.exportExcelBTN.Location = new System.Drawing.Point(530, 472);
+            this.exportExcelBTN.Location = new System.Drawing.Point(821, 650);
             this.exportExcelBTN.Name = "exportExcelBTN";
             this.exportExcelBTN.Size = new System.Drawing.Size(240, 69);
             this.exportExcelBTN.TabIndex = 2;
@@ -85,15 +95,53 @@
             series1.Legend = "Legend1";
             series1.Name = "stocks";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(758, 454);
+            this.chart1.Size = new System.Drawing.Size(617, 388);
             this.chart1.TabIndex = 3;
             this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(635, 12);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.Label = "#VAL";
+            series2.Legend = "Legend1";
+            series2.Name = "stocks";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(617, 388);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(12, 407);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series3.Label = "#VAL";
+            series3.Legend = "Legend1";
+            series3.Name = "stocks";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(617, 388);
+            this.chart3.TabIndex = 5;
+            this.chart3.Text = "chart3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.ClientSize = new System.Drawing.Size(1262, 807);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.exportExcelBTN);
             this.Controls.Add(this.exportImageBTN);
@@ -102,6 +150,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -111,7 +161,9 @@
         private System.Windows.Forms.Button drawChartBTN;
         private System.Windows.Forms.Button exportImageBTN;
         private System.Windows.Forms.Button exportExcelBTN;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart3;
     }
 }
 
--- D10_Export_excel_csv_and_chart/D10-2_Statistic_Chart/D10-2_Statistic_Chart/Form1.cs
+++ D10_Export_excel_csv_and_chart/D10-2_Statistic_Chart/D10-2_Statistic_Chart/Form1.cs
@@ -78,5 +78,6 @@ namespace D10_2_Statistic_Chart
                 xls.Quit();
             }
         }
+
     }
 }
